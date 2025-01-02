3f5db0558e1de3816ca20ab9a30e71c2de1844c0
Tejas Nasre <tejasnasre120@gmail.com>
2025-01-02 23:11:44 +0530

    fix a lil bit navbar need to fix just onopen in mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { Link, NavLink } from "react-router";
-import { Button } from "../ui/button";
-import { useAuthStore } from "@/store/authStore";
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router';
+import { Button } from '../ui/button';
+import { useAuthStore } from '@/store/authStore';
 
 const Navbar: React.FC = () => {
   const [isSticky, setIsSticky] = useState(false);
@@ -9,8 +9,7 @@ const Navbar: React.FC = () => {
   const user = useAuthStore((state) => state.user);
   const fetchSession = useAuthStore((state) => state.fetchSession);
 
-
-  //this useeffect for checking window resize. Bug is when toggle to fullscreen mode while the nav sidebar open from resopnsive mode , the navbar breaks due to improper menuopen stateupdate. 
+  //this useeffect for checking window resize. Bug is when toggle to fullscreen mode while the nav sidebar open from resopnsive mode , the navbar breaks due to improper menuopen stateupdate.
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
@@ -20,11 +19,11 @@ const Navbar: React.FC = () => {
       }
     };
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
     // Cleanup on unmount
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, [menuOpen]); // Dependency on menuOpen to ensure proper updates
 
@@ -36,52 +35,41 @@ const Navbar: React.FC = () => {
     const handleScroll = () => {
       setIsSticky(window.scrollY > 0);
     };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  useEffect
+  const active = `${!menuOpen ? 'p-2 p-2 rounded-full bg-[#2281FF] text-black transition-all duration-300 ease-in-out' : 'p-2 w-full rounded-md text-black border-2 border-black transition-all duration-300 ease-in-out'}`;
 
-
-  const active =
-    `${!menuOpen ? "p-2  rounded-md text-black  transition-all duration-300 ease-in-out" : "p-2 w-full rounded-md text-black border-2 border-black  transition-all duration-300 ease-in-out"}`
-  // "p-2  rounded-md text-black  transition-all duration-300 ease-in-out";
-  const inactive =
-    `${!menuOpen ? "p-2 rounded-full text-black hover:bg-[#2281FF] hover:text-white hover:shadow-md transition-all duration-300 ease-in-out" : "p-2 hover:text-white rounded-full text-black transition-all duration-300 ease-in-out"}`
+  const inactive = `${!menuOpen ? 'p-2 rounded-full text-black hover:bg-[#2281FF] hover:text-white hover:shadow-md transition-all duration-300 ease-in-out' : 'p-2 hover:text-white rounded-full text-black transition-all duration-300 ease-in-out'}`;
 
   return (
     <div
-      className={`font-SpaceGrotesk  font-bold w-full ${menuOpen ? "text-3xl  text-black flex-row-reverse " : ""} pt-4 pb-2 px-6 flex justify-between items-center transition-all duration-300 ${isSticky ? "lg:bg-transparent bg-blue-500  z-50" : "lg:bg-[#FCFFDA] bg-blue-500"
-        }`}
-      style={{ position: "sticky", top: 0, zIndex: 50 }}
-    // onResize={()=>console.log("resized")}
-    // onResizeCapture={()=>console.log("resized")}
+      className={`font-SpaceGrotesk font-bold w-full px-6 pt-8 pb-2 flex flex-row justify-between md:justify-center items-center transition-all duration-300 ${menuOpen ? 'text-3xl text-black flex-row-reverse ' : ''} ${isSticky ? 'bg-transparent z-50' : 'bg-[#FCFFDA]'}`}
+      style={{ position: 'sticky', top: 0, zIndex: 50 }}
     >
       {/* Hamburger Menu (Mobile) */}
-      <div className="lg:hidden mr-4 z-50  top-4 right-4">
-        <button
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="text-3xl focus:outline-none"
-        >
+      <div className="lg:hidden mr-4 z-50 top-4 right-4">
+        <button onClick={() => setMenuOpen(!menuOpen)} className="text-2xl focus:outline-none">
           {!menuOpen ? <>☰</> : <>x</>}
         </button>
       </div>
 
       {/* Logo */}
-      <div className={menuOpen ? "opacity-0" : "ml-4"}>
-        <h1 className={menuOpen ? "hidden" : "ml-4 text-3xl"}>Logo</h1>
+      <div className={menuOpen ? 'opacity-0' : 'ml-4'}>
+        <h1 className={menuOpen ? 'hidden' : 'ml-4 text-3xl'}>Logo</h1>
       </div>
-
 
       {/* Navigation Links */}
       <div
-        className={`fixed top-0 left-0 h-full w-full  lg:static lg:block lg:w-auto lg:shadow-none lg:border-black lg:border-4 lg:rounded-full transition-transform duration-300 ease-in-out transform ${menuOpen
-          ? "translate-x-0 opacity-100 visible p-6 bg-blue-500"
-          : "-translate-x-full opacity-0 invisible pt-1 lg:translate-x-0 lg:opacity-100 lg:visible"
-          }  ${isSticky ? "bg-[#FCFFDA]" : ""} `}
+        className={`fixed top-0 left-0 h-full w-full p-2 lg:static lg:block lg:w-auto lg:shadow-none lg:border-black lg:border-2 lg:rounded-full transition-transform duration-300 ease-in-out transform ${
+          menuOpen
+            ? 'translate-x-0 opacity-100 visible p-6 bg-blue-500'
+            : 'ml-44 mr-16 -translate-x-full opacity-0 invisible lg:translate-x-0 lg:opacity-100 lg:visible'
+        }  ${isSticky ? 'bg-[#FCFFDA]' : ''} `}
         style={{ zIndex: 40 }} // Ensure it appears below the hamburger menu button
       >
-        <div className={`flex ${menuOpen ? "mt-8" : "mt-0"}  flex-col lg:flex-row lg:gap-4 items-start h-full w-full`}>
+        <div className={`flex ${menuOpen ? 'mt-8' : 'mt-0'} flex-col lg:flex-row lg:gap-4 items-start h-full w-full`}>
           <NavLink
             to="/"
             className={({ isActive }) => (isActive ? active : inactive)}
@@ -135,64 +123,50 @@ const Navbar: React.FC = () => {
               </Button>
             ) : (
               <div className="flex flex-col ml-4 pb-5 gap-5">
-                <Button
-                  variant="primary"
-                  size="default"
-                  className=" p-6 flex flex-row text-lg  transition-all"
-                >
+                <Button variant="secondary" size="default" className=" p-6 flex flex-row text-lg  transition-all">
                   <Link to="/signin" onClick={() => setMenuOpen(false)}>
-                    Sign In{" "}
-                    <div
-                      className="text-xl font-bold inline-block transition-transform duration-300 ease-in-out group-hover:rotate-60"
-                    >
+                    Sign In{' '}
+                    <div className="text-xl font-bold inline-block transition-transform duration-300 ease-in-out group-hover:rotate-60">
                       →
                     </div>
                   </Link>
                 </Button>
-                <Button
-                  variant="primary"
-                  size="default"
-                  className="p-6 text-lg  transition-all"
-                >
+                <Button variant="primary" size="default" className="p-6 text-lg  transition-all">
                   <Link to="/signup" onClick={() => setMenuOpen(false)}>
-                    Sign Up{" "}
-                    <span
-                      className="text-xl font-bold inline-block transition-transform duration-300 ease-in-out group-hover:rotate-60"
-                    >
+                    Sign Up{' '}
+                    <span className="text-xl font-bold inline-block transition-transform duration-300 ease-in-out group-hover:rotate-60">
                       →
                     </span>
                   </Link>
                 </Button>
               </div>
-
             )}
           </div>
         </div>
       </div>
 
-
-
-
-
       {/* Action Buttons (Desktop) */}
       <div className="hidden lg:flex lg:gap-4 lg:items-center">
         {user ? (
           <Button variant="primary" className="p-4">
-            <Link to="/profile">Profile</Link>{" "}
-            <span className="text-xl font-bold">→</span>
+            <Link to="/profile">Profile</Link> <span className="text-xl font-bold">→</span>
           </Button>
         ) : (
           <>
-            <div className="" >
-              <div className="flex flex-row gap-2 px-3" >
-                <Button variant="primary" className="p-4">
-                  <Link to="/signin">Sign In</Link>{" "}
-                  <span className="text-xl font-bold">→</span>
-                </Button>
-                <Button variant="primary" className="p-4">
-                  <Link to="/signup">Sign Up</Link>{" "}
-                  <span className="text-xl font-bold">→</span>
-                </Button>
+            <div className="">
+              <div className="flex flex-row gap-4 px-3">
+                <Link to="/signin">
+                  <Button variant="secondary" className="p-4">
+                    Sign In
+                    <span className="text-xl font-bold">→</span>
+                  </Button>
+                </Link>
+                <Link to="/signup">
+                  <Button variant="primary" className="p-4">
+                    Sign Up
+                    <span className="text-xl font-bold">→</span>
+                  </Button>
+                </Link>
               </div>
             </div>
           </>
